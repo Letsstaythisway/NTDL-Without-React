522f66c6a608bc2ea579f745fa2a98c0dc955632
Letsstaythisway <subodh.ranabhat1@gmail.com>
2025-01-07 11:29:45 +1100

    working on danger button

--- script.js
+++ script.js
@@ -8,7 +8,50 @@ const handleOnSubmit = (e) => {
   const obj = {
     task,
     hr,
+    id: randomIdGenerator(),
   };
   taskList.push(obj);
+  displayEntryList();
+};
+
+const displayEntryList = () => {
   console.log(taskList);
+
+  let str = "";
+  const entryElm = document.getElementById("entryList");
+  taskList.map((item, i) => {
+    str += ` <tr>
+                  <td>${i + 1}</td>
+                  <td>${item.task}</td>
+                  <td>${item.hr}</td>
+                  <td class="text-end">
+                    <button onclick="handleOnDelete('${
+                      item.id
+                    }')" class="btn btn-danger">
+                      <i class="fa-regular fa-trash-can"></i>
+                    </button>
+                    <button class="btn btn-success">
+                      <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                  </td>
+                </tr>`;
+  });
+
+  entryElm.innerHTML = str;
+};
+
+const randomIdGenerator = (length = 6) => {
+  const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+
+  let id = "";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * str.length);
+    id += str[randomIndex];
+  }
+  return id;
+};
+
+const handleOnDelete = (id) => {
+  console.log(id);
 };
